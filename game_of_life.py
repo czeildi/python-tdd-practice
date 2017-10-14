52f9fb8243b0426bc0c8f8bbdae131fb7409158a
Ildiko Czeller <czeildi@gmail.com>
2017-10-14 19:02:34 +0200

    refactor to communicate logic

--- game_of_life.py
+++ game_of_life.py
@@ -2,4 +2,4 @@ class GameOfLife:
     """implement Conway's game of life"""
 
     def next(self, living_cells = []):
-        return [cell for cell in living_cells if cell == '0, 1']
+        return living_cells[1:2]
